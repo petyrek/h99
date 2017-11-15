2675a223b7fa49034ec658c6ea5d3d474ace70cf
Martin Petyrek <petyrekm@gmail.com>
2017-11-15 18:27:35 +0100

    finished 11-20

--- q01_10.hs
+++ q01_10.hs
@@ -1,35 +1,48 @@
 import Data.List
 
+-- PROBLEM 1 --
 myLast :: [a] -> a
 myLast [x] = x
 myLast (x:xs) = myLast xs
 
+-- PROBLEM 2 --
 myButLast :: [a] -> a
 myButLast (x:xs) = if length xs == 1 then x
                    else myButLast xs
 
+-- PROBLEM 3 --
 elementAt :: [a] -> Int -> a
 elementAt (x:xs) 1 = x
 elementAt (x:xs) n = elementAt xs (n - 1)
 
+-- PROBLEM 5 --
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
+-- PROBLEM 6 --
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome [] = True
 isPalindrome [_] = True
 isPalindrome (x:xs) = x == last xs && isPalindrome (init xs)
 
+-- PROBLEM 7 --
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
 flatten (Elem a) = [a]
 flatten (List (x:xs)) = flatten x ++ flatten (List xs)
 flatten (List []) = []
 
+-- PROBLEM 8 --
 compress :: Eq a => [a] -> [a]
 compress []     = []
 compress (x:xs) = x : (compress $ dropWhile (== x) xs)
 
+-- PROBLEM 9 --
+pack :: [a] -> [[a]]
+pack (x:xs) = x:(takeWhile (==x) xs) : pack (dropWhile (==x) xs)
+
+
+-- PROBLEM 10 --
 encode :: Eq a => [a] -> [(Int, a)]
 encode = map (\x -> (length x, head x)) . group
--- q11_20.hs
+++ q11_20.hs
@@ -3,6 +3,7 @@ import Data.List
 data Entry a = Single a | Multiple Int a
   deriving (Show)
 
+-- PROBLEM 11 --
 encodeModified :: Eq a => [a] -> [Entry a]
 encodeModified = map getEntry . group
     where
@@ -10,9 +11,49 @@ encodeModified = map getEntry . group
                then Single (head x)
                else Multiple (length x) (head x)
 
-
+-- PROBLEM 12 --
 decodeModified :: [Entry a] -> [a]
 decodeModified = concatMap decodeHelper
     where
       decodeHelper (Single a) = [a]
       decodeHelper (Multiple n a) = replicate n a
+
+-- PROBLEM 13 --
+encodeDirect :: Eq a => [a] -> [Entry a]
+encodeDirect [] = []
+encodeDirect (x:xs) = encode (x : takeWhile (==x) xs) : encodeDirect (dropWhile (==x) xs)
+      where encode [x]    = Single x
+            encode (x:xs) = Multiple (length xs + 1) x
+
+-- PROBLEM 14 --
+dupli :: [a] -> [a]
+dupli = concatMap(\x -> [x, x])
+
+-- PROBLEM 15 --
+repli :: [a] -> Int -> [a]
+repli xs n = concatMap (replicate n) xs
+
+-- PROBLEM 16 --
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n = helper xs n
+      where helper [] _ = []
+            helper (x:xs) 1 = helper xs n
+            helper (x:xs) k = x:helper xs (k-1)
+
+-- PROBLEM 17 --
+split :: [a] -> Int -> ([a], [a])
+split xs n = (take n xs, drop n xs)
+
+-- PROBLEM 18 --
+slice :: [a] -> Int -> Int -> [a]
+slice xs i k = take (k-i+1) (drop (i-1) xs)
+
+-- PROBLEM 19 --
+rotate :: [a] -> Int -> [a]
+rotate xs n = if n > 0 then rotateRight xs n else rotateLeft xs (-n)
+      where rotateRight xs n = drop n xs ++ take n xs
+            rotateLeft  xs n = drop (length xs - n) xs ++ take (length xs - n) xs
+
+-- PROBLEM 20 --
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n xs = (xs !! (n-1), take (n-1) xs ++ drop n xs)
